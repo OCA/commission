077d76ca401f35ca430aca7bba01699b301fcce7
Giorgio Borelli <casaborelli@gmail.com>
2014-11-06 12:13:10 +0100

    [MIG] sale_commission: Migration to 8.0
    * migrate models security and i18n
    * add demo data and minor fixes
    * add sale agent and commission views
    * add stock dependency add views and product/stock overrides
    * fixed related fields
    * minor fixes: names and labels
    * change 'onchange' attribute in view to api.onchange'
    * improve comments about migration
    * add invoice view
    * add wizards
    * use api.onchange
    * add store attribute on res.partner.agent
    * set default currency
    * change state field to readonly
    * migrate stock picking and settlements views
    * fix invoice view
    * fix invoice wizard
    * change osv.except_osv to openerp.exceptions
    * add missed report
    * fix taxes in action_invoice_create

---
+++ sale_commission/model/partner.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2011 Pexego Sistemas Inform??ticos (<http://www.pexego.es>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp import models, fields
+
+
+class res_partner(models.Model):
+    """Add some fields related to commissions"""
+
+    _inherit = "res.partner"
+
+    commission_ids = fields.One2many(
+        "res.partner.agent",
+        "partner_id",
+        string="Agents"
+    )
+
+    agent = fields.Boolean(
+        string="Creditor/Agent",
+        help="If you check this field will be available as creditor or agent."
+    )
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
