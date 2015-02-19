589225125acbd921c61530d63587a3dc3d554123
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-02-19 10:12:38 +0100

    [RFR] sale_commission: Total refactorization of the modules
    * Take default commission when manual adding agents + error on settled filter
    * Many2one pointer of comission section
    * Fix translation problem + add commission total on sales orders
    * Cancel settlement when the associated invoice is cancel and go back to invoiced when re-validated

---
+++ sale_commission/models/sale_commission.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2011 Pexego Sistemas Informáticos (<http://www.pexego.es>).
+#    Copyright (C) 2015 Avanzosc (<http://www.avanzosc.es>)
+#    Copyright (C) 2015 Pedro M. Baeza (<http://www.serviciosbaeza.com>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp import models, fields, api, exceptions, _
+
+
+class SaleCommission(models.Model):
+    _name = "sale.commission"
+    _description = "Commission in sales"
+
+    name = fields.Char('Name', required=True)
+    commission_type = fields.Selection(
+        selection=[("fixed", "Fixed percentage"),
+                   ("section", "By sections")],
+        string="Type", required=True, default="fixed")
+    fix_qty = fields.Float(string="Fixed percentage")
+    sections = fields.One2many(
+        comodel_name="sale.commission.section", inverse_name="commission")
+    active = fields.Boolean(default=True)
+
+    @api.multi
+    def calculate_section(self, base):
+        self.ensure_one()
+        for section in self.sections:
+            if section.amount_from <= base <= section.amount_to:
+                return base * section.percent / 100.0
+        return 0.0
+
+
+class SaleCommissionSection(models.Model):
+    _name = "sale.commission.section"
+    _description = "Commission section"
+
+    commission = fields.Many2one("sale.commission", string="Commission")
+    amount_from = fields.Float(string="From")
+    amount_to = fields.Float(string="To")
+    percent = fields.Float(string="Percent", required=True)
+
+    @api.one
+    @api.constrains('amount_from', 'amount_to')
+    def _check_amounts(self):
+        if self.amount_to < self.amount_from:
+            raise exceptions.ValidationError(
+                _("The lower limit cannot be greater than upper one."))
