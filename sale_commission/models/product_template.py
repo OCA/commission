49b6922be0603e16b80a0a9252307125ba90e5d6
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-02-19 10:12:38 +0100

    [RFR] sale_commission: Total refactorization of the modules
    * Take default commission when manual adding agents + error on settled filter
    * Many2one pointer of comission section
    * Fix translation problem + add commission total on sales orders
    * Cancel settlement when the associated invoice is cancel and go back to invoiced when re-validated

--- sale_commission/models/product_template.py
+++ sale_commission/models/product_template.py
@@ -3,6 +3,8 @@
 #
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2011 Pexego Sistemas Informáticos (<http://www.pexego.es>).
+#    Copyright (C) 2015 Avanzosc (<http://www.avanzosc.es>)
+#    Copyright (C) 2015 Pedro M. Baeza (<http://www.serviciosbaeza.com>)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -21,8 +23,8 @@
 from openerp import models, fields
 
 
-class ProductProduct(models.Model):
-    _inherit = 'product.product'
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
 
     commission_free = fields.Boolean(string="Free of commission",
                                      default=False)
