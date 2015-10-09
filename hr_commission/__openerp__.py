12cac9a08c896175faa1715b82b36d18ea81753b
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-09 09:59:42 +0200

    [UPD] prefix versions with 8.0

--- hr_commission/__openerp__.py
+++ hr_commission/__openerp__.py
@@ -20,7 +20,7 @@
 
 {
     'name': 'HR commissions',
-    'version': '1.0',
+    'version': '8.0.1.0.0',
     'author': 'Serv. Tecnol. Avanzados - Pedro M. Baeza',
     "category": "Human Resources",
     'depends': [
