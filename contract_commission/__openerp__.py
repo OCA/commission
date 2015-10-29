89181fcccdd9ba1a39c0f7afab1cc821fc40bd06
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-10-29 12:15:56 +0100

    contract_commission
    =======================
    Commission in contracts
    =======================
    
    This module allows to get default commissions for partner in the recurring
    invoices generated from contracts.
    
    Installation
    ============
    
    This module is auto-installed when you have *account_analytic_analysis* and
    *sale_commission* modules installed.
    
    Usage
    =====
    
    When invoicing contracts, the agents assigned to the partner of the contract
    will now be assigned to the generated invoices.

---
+++ contract_commission/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# (c) 2015 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'Commissions in contract invoices',
+    'version': '8.0.1.0.0',
+    'author': 'Serv. Tecnol. Avanzados - Pedro M. Baeza, '
+              'Odoo Community Association (OCA)',
+    "category": "Generic Modules/Sales & Purchases",
+    'license': 'AGPL-3',
+    'depends': [
+        'account_analytic_analysis',
+        'sale_commission',
+    ],
+    'contributors': [
+        "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
