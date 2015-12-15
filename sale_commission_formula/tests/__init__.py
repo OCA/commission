8c10dfc5c7b353efe848be798495eca8b1979ba1
Nicola Malcontenti <nicola.malcontenti@agilebg.com>
2015-12-15 10:41:39 +0100

    MV sale commission formula to updated module

---
+++ sale_commission_formula/tests/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# © 2016 Nicola Malcontenti - Agile Business Group
+# © 2016 Davide Corio - Abstract
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from . import test_commission_formula
