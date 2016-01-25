bf6de35c8232e8be2d6178caca23e0400ee5d05b
Iván Todorovich <ivan.todorovich@gmail.com>
2016-01-25 15:05:05 -0300

    Fix refunds

--- sale_commission/__openerp__.py
+++ sale_commission/__openerp__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Sales commissions',
-    'version': '8.0.2.2.1',
+    'version': '8.0.2.2.2',
     'author': 'Pexego, '
               'Savoire-faire linux, '
               'Avanzosc, '
@@ -30,6 +30,7 @@
         "Daniel Campos <danielcampos@avanzosc.es>",
         "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>",
         "Oihane Crucelaegui <oihanecruce@gmail.com>",
+        "Iván Todorovich <ivan.todorovich@gmail.com",
     ],
     "data": [
         "security/ir.model.access.csv",
