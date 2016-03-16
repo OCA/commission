5e66eef4c465e5a117af15554865d5339d507a3f
Alejandro-Asr <alejandro@asr-oss.com>
2016-03-16 11:07:10 +0100

    multiple changes

--- sale_commission_product/__openerp__.py
+++ sale_commission_product/__openerp__.py
@@ -1,23 +1,6 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (c) 2015 Asr Oss (http://www.asr-oss.com)
-#                       Alejandro Sanchez Ramirez <alejandro@asr-oss.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2015 Alejandro Sánchez Ramírez (<http://www.asr-oss.com>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     'name': 'Sale commissions product',
@@ -28,6 +11,7 @@
     'license': 'AGPL-3',
     'depends': [
         'sale_commission',
+        'web_widget_one2many_tags',
     ],
     'contributors': [
         "Alejandro Sánchez <alejandro@asr-oss.com>",
