e8234c86c8ffd39a306354e355b8852a42ceac4b
javierjcf <javierjcf@gmail.com>
2016-04-06 18:46:30 +0200

    [ADD]commission_by_partner: Módulo para tener distintas comisiones en cada agente para cada cliente

---
+++ commission_by_partner/README.rst
@@ -0,0 +1,42 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+=================
+Sales commissions by partner
+=================
+
+This module allows to define different commissiont to an angent in each partner.
+
+
+
+Known issues / Roadmap
+======================
+
+
+Bug Tracker
+===========
+
+
+
+Credits
+=======
+
+Contributors
+------------
+* Comunitea
+* Javier Colmenero <javier@comunitea.com>
+* Joao Alfredo Gama Batista <joao.gama@savoirfairelinux.com>
+* Sandy Carter <sandy.carter@savoirfairelinux.com>
+* Giorgio Borelli <giorgio.borelli@abstract.it>
+* Daniel Campos <danielcampos@avanzosc.es>
+* Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
+* Oihane Crucelaegui <oihanecruce@gmail.com>
+
+Icon
+----
+
+
+Maintainer
+----------
+This module is maintained by Comunitea.
---
+++ commission_by_partner/__openerp__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 Comunitea Servicios Tecnologicos Sl.All Rights Reserved
+#    $Javier Colmenero Fernández$ <javier@comunitea.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Sale commissions by partner',
+    'version': '8.0.2.4.0',
+    'author': 'Comunitea',
+    "category": "Sales Management",
+    'license': 'AGPL-3',
+    'depends': [
+        'sale_commission',
+    ],
+    'contributors': [
+        "Comunitea",
+        "Javier Colmenero Fernández <javier@comunitea.com>",
+    ],
+    "data": [
+    ],
+    "demo": [
+    ],
+    'test': [
+    ],
+    "installable": True
+}
