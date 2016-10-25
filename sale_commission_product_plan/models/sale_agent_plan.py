e68e0dcfa2b33257c76ecd41301fd80532c49212
Omar <omarcs7r@gmail.com>
2016-10-25 18:55:51 +0200

    [IMP] sale_commission_product_plan: Controls the unlink of plan object and allows to copy line in duplicate action

--- sale_commission_product_plan/models/sale_agent_plan.py
+++ sale_commission_product_plan/models/sale_agent_plan.py
@@ -2,7 +2,7 @@
 # © 2016 Comunitea
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields, api
+from openerp import models, fields, api, exceptions, _
 
 
 class SaleAgentPlan(models.Model):
@@ -10,7 +10,7 @@ class SaleAgentPlan(models.Model):
     _name = 'sale.agent.plan'
 
     name = fields.Char(required=True)
-    lines = fields.One2many('sale.agent.plan.line', 'plan')
+    lines = fields.One2many('sale.agent.plan.line', 'plan', copy=True)
 
     @api.multi
     def get_product_commission(self, product=False):
@@ -20,6 +20,16 @@ class SaleAgentPlan(models.Model):
             commission_line = self.lines.filtered(
                 lambda r: r.product.id is False)
         return commission_line.commission
+
+    @api.multi
+    def unlink(self):
+        for plan in self:
+            partner_ids = self.env["res.partner"].\
+                search([('plan', '=', plan.id)])
+            if partner_ids:
+                raise exceptions.Warning(_("Cannot delete this plan, because "
+                                           "it is related to partners"))
+        return super(SaleAgentPlan, self).unlink()
 
 
 class SaleAgentPlanLine(models.Model):
