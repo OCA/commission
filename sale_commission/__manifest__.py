3fed1abb3bc66cd4b377c474bc56ca72de7c2e93
Khaled Hamedq <kholioeg@gmail.com>
2017-05-05 03:04:34 +0200

    This is the port for V10 branch. Branch 10.0. Tested on V10C. Porting has been done on the following files: 	modified:   __manifest__.py 	modified:   models/account_invoice.py 	modified:   models/settlement.py 	modified:   security/ir.model.access.csv 	modified:   views/account_invoice_view.xml 	modified:   views/res_partner_view.xml 	modified:   views/sale_commission_view.xml 	modified:   views/sale_order_view.xml 	modified:   views/settlement_view.xml 	modified:   wizard/wizard_settle.xml

--- sale_commission/__manifest__.py
+++ sale_commission/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Sales commissions',
-    'version': '8.0.2.3.0',
+    'version': '10.0',
     'author': 'Pexego, '
               'Savoire-faire linux, '
               'Avanzosc, '
@@ -19,7 +19,8 @@
         'base',
         'account',
         'product',
-        'sale'
+        'sale',
+        # 'web_widget_one2many_tags',
     ],
     'contributors': [
         "Pexego",
@@ -50,5 +51,6 @@
     'test': [
         'test/sale_commission_demo.yml',
     ],
-    'installable': False
+    'installable': True,
+    # "pre_init_hook": "set_commission_total",
 }
