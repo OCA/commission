3e0830d953ea328556850386260c6e4fb6c54830
Nicola Malcontenti <nicola.malcontenti@agilebg.com>
2017-05-05 16:02:47 +0200

    [MIG] Porting Sale commission formula to version 10 !

--- sale_commission_formula/models/sale_commission.py
+++ sale_commission_formula/models/sale_commission.py
@@ -3,7 +3,7 @@
 # © 2016 Davide Corio - Abstract
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class Commission(models.Model):
