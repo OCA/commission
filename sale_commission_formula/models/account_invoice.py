a9967ab08c05546fdc76bfeaf2feb066bc19d459
rgarnau <rgarnau@gmail.com>
2017-07-13 13:14:37 +0200

    add method to define formula parameters to be used

--- sale_commission_formula/models/account_invoice.py
+++ sale_commission_formula/models/account_invoice.py
@@ -10,6 +10,11 @@ from odoo.tools.safe_eval import safe_eval
 class AccountInvoiceLineAgent(models.Model):
     _inherit = 'account.invoice.line.agent'
 
+    @api.model
+    def _get_formula_input_dict(self):
+        return {'line': self.sale_line,
+                'self': self}
+
     @api.depends('commission.commission_type', 'invoice_line.price_subtotal',
                  'commission.amount_base_type')
     def _compute_amount(self):
@@ -19,7 +24,7 @@ class AccountInvoiceLineAgent(models.Model):
                     line_obj.commission):
                 line_obj.amount = 0.0
                 formula = line_obj.commission.formula
-                results = {'line': line_obj.invoice_line}
+                results = line_obj._get_formula_input_dict()
                 safe_eval(formula, results, mode="exec", nocopy=True)
                 line_obj.amount += float(results['result'])
             else:
