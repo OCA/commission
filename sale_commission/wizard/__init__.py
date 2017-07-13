c2340f4f455e0dff66b264e6f58d82b8df5c5d7c
rgarnau <rgarnau20@gmail.com>
2017-07-13 16:42:40 +0200

    add method to prepare agents
    add same method for account invoice

--- sale_commission/wizard/__init__.py
+++ sale_commission/wizard/__init__.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-# © 2011 Pexego Sistemas Informáticos (<http://www.pexego.es>)
-# © 2015 Pedro M. Baeza (<http://www.serviciosbaeza.com>)
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import wizard_invoice
 from . import wizard_settle
