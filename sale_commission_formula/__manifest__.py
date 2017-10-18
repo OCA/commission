e738fffcad4dc93e6b4faf50a43a216c8234f424
etobella <etobella@creublanca.es>
2017-10-18 15:53:28 +0200

    [MIG] sale_commission_formula: Migration to 11.0

--- sale_commission_formula/__manifest__.py
+++ sale_commission_formula/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # © 2016 Nicola Malcontenti - Agile Business Group
 # © 2016 Davide Corio - Abstract
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+# License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
 
 {
     'name': 'Sale Commission Formula',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Sale',
     'license': 'AGPL-3',
     'summary': 'Sale commissions computed by formulas',
--- sale_commission_formula/tests/__init__.py
+++ sale_commission_formula/tests/__init__.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
 # © 2016 Nicola Malcontenti - Agile Business Group
 # © 2016 Davide Corio - Abstract
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+# License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
 
 from . import test_commission_formula
