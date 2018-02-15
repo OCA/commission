3a363cf84649ebf664696e7ba949df322269cc7c
Lucas Huber <lucas.huber@org-it.ch>
2018-02-15 14:21:21 +0100

    [MIG] sale_commission_formula: Migration to 10.0

---
+++ sale_commission_formula/__manifest__.py
@@ -0,0 +1,23 @@
+# © 2016 Nicola Malcontenti - Agile Business Group
+# © 2016 Davide Corio - Abstract
+# License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'Sale Commission Formula',
+    'version': '10.0.1.0.0',
+    'category': 'Sale',
+    'license': 'AGPL-3',
+    'summary': 'Sale commissions computed by formulas',
+    'author': "Abstract,Odoo Community Association (OCA)",
+    'website': 'http://www.abstract.it',
+    'depends': [
+        'sale_commission'],
+    'data': [
+        'views/sale_commission_view.xml',
+        'data/commission_data.xml'
+           ],
+    'demo': [
+        'demo/commission_demo.xml'
+            ],
+    'installable': True,
+}
