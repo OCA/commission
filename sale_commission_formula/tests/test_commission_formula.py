99450a4ef50b3ae889969c9c1098468e98d097e1
Lucas Huber <lucas.huber@org-it.ch>
2018-02-16 11:39:42 +0100

    [MIG] sale_commission_formula: Migration to 10.0 fixed test

--- sale_commission_formula/tests/test_commission_formula.py
+++ sale_commission_formula/tests/test_commission_formula.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # © 2016 Nicola Malcontenti - Agile Business Group
 # © 2016 Davide Corio - Abstract
-# License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo.tests.common import TransactionCase
 
@@ -14,7 +14,7 @@ class TestCommissionFormula(TransactionCase):
         self.commission = self.env.ref(
             'sale_commission_formula.commission_5perc10extra')
         self.sale_order = self.env.ref('sale_commission_formula.sale_order_1')
-        self.so_line = self.env.ref(
+        self.sale_order_line = self.env.ref(
             'sale_commission_formula.sale_order_line_1')
         self.agent_commissions = {
             'agent': self.agent.id, 'commission': self.commission.id}
@@ -22,25 +22,16 @@ class TestCommissionFormula(TransactionCase):
     def test_sale_order_commission(self):
         # we test the '5% + 10% extra' we should return 41.25 since
         # the order total amount is 750.00.
-        self.env['sale.order.line.agent'].create({
-            'sale_line': self.so_line.id,
-            'agent': self.agent.id,
-            'commission': self.commission.id,
-        })
+        self.sale_order_line.agents = [(0, 0, self.agent_commissions)]
         self.assertEqual(41.25, self.sale_order.commission_total)
 
     def test_invoice_commission(self):
         # we confirm the sale order and create the corresponding invoice
-        self.sale_order.action_confirm()
-        self.so_line.qty_delivered = self.so_line.product_uom_qty
+        self.sale_order.action_button_confirm()
         invoice_id = self.sale_order.action_invoice_create()
         invoice = self.env['account.invoice'].browse(invoice_id)
         # we add the commissions on the first invoice line
-        invoice_line = invoice.invoice_line_ids[0]
-        self.env['account.invoice.line.agent'].create({
-            'invoice_line': invoice_line.id,
-            'agent': self.agent.id,
-            'commission': self.commission.id,
-        })
+        invoice_line = invoice.invoice_line[0]
+        invoice_line.agents = [(0, 0, self.agent_commissions)]
         # we test the '5% + 10% extra' commissions on the invoice too
         self.assertEqual(41.25, invoice.commission_total)
