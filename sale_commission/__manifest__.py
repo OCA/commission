f5eab33fc5269572f5e82a74e208f0e73c7b4f25
Enric Tobella <etobella@creublanca.es>
2018-05-16 13:50:36 +0200

    [IMP] Add hook on make invoice settlement

--- sale_commission/__manifest__.py
+++ sale_commission/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Sales commissions',
-    'version': '11.0.1.2.1',
+    'version': '11.0.1.3.0',
     'author': 'Odoo Community Association (OCA)',
     'category': 'Sales Management',
     'license': 'AGPL-3',
