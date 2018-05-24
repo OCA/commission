1a77d6f1a79104e1523706e54f668355f7b98912
eLBati <lorenzo.battistini@agilebg.com>
2018-05-24 16:38:06 +0200

    porting sale_commission_pricelist to 11.0

--- sale_commission_pricelist/__manifest__.py
+++ sale_commission_pricelist/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Carlos Dauden - Tecnativa <carlos.dauden@tecnativa.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Sales commissions by pricelist',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'author': 'Tecnativa, '
               'Odoo Community Association (OCA)',
     'category': 'Sales',
--- sale_commission_pricelist/models/__init__.py
+++ sale_commission_pricelist/models/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 
 from . import product_pricelist
 from . import sale_order
