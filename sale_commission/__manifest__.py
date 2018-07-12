59878ea5cdcd20b0d09fd2e6e6b0a361017bc9ff
eLBati <lorenzo.battistini@agilebg.com>
2018-07-12 14:48:46 +0200

    FIX sale_commission the following use case
     - Go to Sales -> Customers
     - Filters -> Add Custom Filter: Agents contains test
    
    Get
    
    odoo/addons/base/res/res_partner.py", line 647, in name_search
        where_query = self._where_calc(args)
    odoo/models.py", line 4038, in _where_calc
        domain = [('active', '=', 1)] + domain
    TypeError: can only concatenate list (not "str") to list

--- sale_commission/__manifest__.py
+++ sale_commission/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Sales commissions',
-    'version': '11.0.1.3.0',
+    'version': '11.0.1.4.0',
     'author': 'Odoo Community Association (OCA)',
     'category': 'Sales Management',
     'license': 'AGPL-3',
