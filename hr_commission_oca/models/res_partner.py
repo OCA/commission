894262bf363aacee04060d21371b075116e260d9
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-08-06 12:17:35 +0200

    [MIG] hr_commission: Migration to 11.0

--- hr_commission_oca/models/res_partner.py
+++ hr_commission_oca/models/res_partner.py
@@ -1,55 +1,37 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2015 Avanzosc (<http://www.avanzosc.es>)
-#    Copyright (C) 2015 Pedro M. Baeza (<http://www.serviciosbaeza.com>)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-from openerp import models, fields, api, exceptions, _
+# Copyright 2015-2018 Tecnativa - Pedro M. Baeza
+# License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import _, api, fields, exceptions, models
 
 
 class ResPartner(models.Model):
-    """Add some fields related to commissions"""
     _inherit = "res.partner"
 
     agent_type = fields.Selection(
-        selection_add=[("salesman", "Salesman (employee)")])
-    employee = fields.Many2one(
-        comodel_name="hr.employee", compute="_get_employee")
-    users = fields.One2many(comodel_name="res.users",
-                            inverse_name="partner_id")
+        selection_add=[("salesman", "Salesman (employee)")],
+    )
+    employee_id = fields.Many2one(
+        comodel_name="hr.employee",
+        compute="_compute_employee_id",
+        compute_sudo=True,
+    )
 
-    @api.one
-    @api.depends('users')
-    def _get_employee(self):
-        self.employee = False
-        if len(self.users) == 1 and len(self.users[0].employee_ids) == 1:
-            self.employee = self.users[0].employee_ids[0]
+    @api.depends('user_ids')
+    def _compute_employee_id(self):
+        for partner in self:
+            if (len(partner.user_ids) == 1 and
+                    len(partner.user_ids[0].employee_ids) == 1):
+                partner.employee_id = partner.user_ids[0].employee_ids[0]
 
-    @api.constrains('agent_type', 'employee')
+    @api.constrains('agent_type')
     def _check_employee(self):
-        if self.agent_type == 'salesman' and not self.employee:
+        if self.agent_type == 'salesman' and not self.employee_id:
             raise exceptions.ValidationError(
                 _("There must one (and only one) employee linked to this "
                   "partner. To do this, go to 'Human Resources' and check "
                   "'Employees'"))
 
     @api.onchange('agent_type')
-    def onchange_agent_type(self):
+    def onchange_agent_type_hr_commission(self):
         if self.agent_type == 'salesman':
             self.supplier = False
-        return super(ResPartner, self).onchange_agent_type()
---
+++ hr_commission_oca/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module links sale_commission with hr module. For now, it only adds another
+type of agent whose commissions are not invoiced in the corresponding wizard.
