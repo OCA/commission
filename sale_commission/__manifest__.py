392b84eed880c805e533f225253fb2537a11c025
eLBati <lorenzo.battistini@agilebg.com>
2018-10-29 17:50:58 +0100

    IMP sale_commission: allow to find partners without assigned agents

--- sale_commission/__manifest__.py
+++ sale_commission/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Sales commissions',
-    'version': '11.0.1.4.3',
+    'version': '11.0.1.5.0',
     'author': 'Odoo Community Association (OCA)',
     'category': 'Sales Management',
     'license': 'AGPL-3',
