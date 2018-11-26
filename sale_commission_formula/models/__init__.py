f0134116857c7ff44ccd96551217fd172e9b2c87
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-11-27 00:49:50 +0100

    [RFR] sale_commission_formula: Adapt module to new structure

--- sale_commission_formula/models/__init__.py
+++ sale_commission_formula/models/__init__.py
@@ -1,7 +1,5 @@
-# © 2016 Nicola Malcontenti - Agile Business Group
-# © 2016 Davide Corio - Abstract
 # License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
 
 from . import account_invoice
 from . import sale_commission
-from . import sale_order
+from . import sale_commission_mixin
