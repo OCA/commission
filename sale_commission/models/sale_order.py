13f0e5ee71cdfa647b430e4b7560c7ae940ce6f4
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-11-26 18:27:16 +0100

    [FIX+IMP] sale_commission: Reworked module
    - Remove onchange hackings as they are not needed with new JS views.
    - Extract common code between sales orders and invoices, creating 2 mixins.
    - Adapt tests.
    - Improve README with detailed explanations.
    - README by fragments.
    - Changed `settled` condition to a more suitable one, moving the condition to _skip_settlement.
    - Improve interface for not removing editable option in lines: you have a new icon that popups
      a window with agents. This icon is available according certain conditions. Check README for
      more details.
    - Add constraint for avoiding the modification of a settled commission.
    - Add agent values when no other value is provided. This way, we add compatibility with
      modules like `sale_product_set` or `sale_order_variant_mgmt`.

--- sale_commission/models/sale_order.py
+++ sale_commission/models/sale_order.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
+# Copyright 2014-2018 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
 
@@ -14,73 +15,44 @@ class SaleOrder(models.Model):
                 record.commission_total += sum(x.amount for x in line.agents)
 
     commission_total = fields.Float(
-        string="Commissions", compute="_compute_commission_total",
-        store=True)
+        string="Commissions",
+        compute="_compute_commission_total",
+        store=True,
+    )
 
-    @api.multi
-    @api.onchange('partner_id')
-    def onchange_partner_id(self):
-        self.ensure_one()
-        res = super(SaleOrder, self).onchange_partner_id()
-        # workaround for https://github.com/odoo/odoo/issues/17618
-        for order_line in self.order_line:
-            order_line.agents = None
-        return res
-
-    @api.onchange('fiscal_position_id')
-    def _compute_tax_id(self):
-        self.ensure_one()
-        res = super(SaleOrder, self)._compute_tax_id()
-        # workaround for https://github.com/odoo/odoo/issues/17618
-        for order_line in self.order_line:
-            order_line.agents = None
-        return res
-
-    @api.model
-    def _prepare_line_agents_data(self, line):
-        rec = []
-        for agent in self.partner_id.agents:
-            rec.append({
-                'agent': agent.id,
-                'commission': agent.commission.id,
-            })
-        return rec
-
-    @api.multi
     def recompute_lines_agents(self):
-        for order in self:
-            for line in order.order_line:
-                line.agents.unlink()
-                line_agents_data = order._prepare_line_agents_data(line)
-                line.agents = [(
-                    0,
-                    0,
-                    line_agent_data) for line_agent_data in line_agents_data]
+        self.mapped('order_line').recompute_agents()
 
 
 class SaleOrderLine(models.Model):
-    _inherit = "sale.order.line"
-
-    @api.model
-    def _default_agents(self):
-        agents = []
-        if self.env.context.get('partner_id'):
-            partner = self.env['res.partner'].browse(
-                self.env.context['partner_id'])
-            for agent in partner.agents:
-                agents.append({'agent': agent.id,
-                               'commission': agent.commission.id})
-        return [(0, 0, x) for x in agents]
+    _inherit = [
+        "sale.order.line",
+        "sale.commission.mixin",
+    ]
+    _name = "sale.order.line"
 
     agents = fields.One2many(
         string="Agents & commissions",
-        comodel_name="sale.order.line.agent", inverse_name="sale_line",
-        copy=True, readonly=True, default=_default_agents)
-    commission_free = fields.Boolean(
-        string="Comm. free", related="product_id.commission_free",
-        store=True, readonly=True)
+        comodel_name="sale.order.line.agent",
+    )
 
-    @api.multi
+    @api.model
+    def create(self, vals):
+        """Add agents for records created from automations instead of UI."""
+        if 'agents' not in vals:
+            order = self.env['sale.order'].browse(vals['order_id'])
+            vals['agents'] = self._prepare_agents_vals_partner(
+                order.partner_id,
+            )
+        return super().create(vals)
+
+    def _prepare_agents_vals(self):
+        self.ensure_one()
+        res = super()._prepare_agents_vals()
+        return res + self._prepare_agents_vals_partner(
+            self.order_id.partner_id,
+        )
+
     def _prepare_invoice_line(self, qty):
         vals = super(SaleOrderLine, self)._prepare_invoice_line(qty)
         vals['agents'] = [
@@ -90,40 +62,27 @@ class SaleOrderLine(models.Model):
 
 
 class SaleOrderLineAgent(models.Model):
+    _inherit = "sale.commission.line.mixin"
     _name = "sale.order.line.agent"
-    _rec_name = "agent"
 
-    sale_line = fields.Many2one(
-        comodel_name="sale.order.line", required=True, ondelete="cascade")
-    agent = fields.Many2one(
-        comodel_name="res.partner", required=True, ondelete="restrict",
-        domain="[('agent', '=', True)]")
-    commission = fields.Many2one(
-        comodel_name="sale.commission", required=True, ondelete="restrict")
-    amount = fields.Float(compute="_compute_amount", store=True)
-
-    _sql_constraints = [
-        ('unique_agent', 'UNIQUE(sale_line, agent)',
-         'You can only add one time each agent.')
-    ]
-
-    @api.onchange('agent')
-    def onchange_agent(self):
-        self.commission = self.agent.commission
-
-    @api.depends('sale_line.price_subtotal')
-    def _compute_amount(self):
-        for line in self:
-            line.amount = 0.0
-            if (not line.sale_line.product_id.commission_free and
-                    line.commission):
-                if line.commission.amount_base_type == 'net_amount':
-                    subtotal = (line.sale_line.price_subtotal -
-                                (line.sale_line.product_id.standard_price *
-                                 line.sale_line.product_uom_qty))
-                else:
-                    subtotal = line.sale_line.price_subtotal
-                if line.commission.commission_type == 'fixed':
-                    line.amount = subtotal * (line.commission.fix_qty / 100.0)
-                else:
-                    line.amount = line.commission.calculate_section(subtotal)
+    object_id = fields.Many2one(
+        comodel_name="sale.order.line",
+        oldname="sale_line",
+    )
+    # Overwritten fields for indicating the source for computing comm. amount
+    source_product_id = fields.Many2one(
+        related="object_id.product_id",
+        readonly=True,
+    )
+    source_quantity = fields.Float(
+        related="object_id.product_uom_qty",
+        readonly=True,
+    )
+    source_total = fields.Monetary(
+        related="object_id.price_subtotal",
+        readonly=True,
+    )
+    currency_id = fields.Many2one(
+        related="object_id.currency_id",
+        readonly=True,
+    )
