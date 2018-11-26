9bd885e7a4657d397932dc63c1327a03d16f61ec
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-11-27 00:49:50 +0100

    [RFR] sale_commission_formula: Adapt module to new structure

--- sale_commission_formula/models/account_invoice.py
+++ sale_commission_formula/models/account_invoice.py
@@ -1,31 +1,21 @@
 # © 2016 Nicola Malcontenti - Agile Business Group
 # © 2016 Davide Corio - Abstract
+# Copyright 2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, api
-from odoo.tools.safe_eval import safe_eval
+from odoo import models
 
 
 class AccountInvoiceLineAgent(models.Model):
     _inherit = 'account.invoice.line.agent'
 
-    @api.model
-    def _get_formula_input_dict(self):
-        return {'line': self.object_id,
-                'self': self}
-
     def _compute_amount(self):
-        for line_obj in self:
-            if (line_obj.commission.commission_type == 'formula' and
-                not line_obj.object_id.product_id.commission_free and
-                    line_obj.commission):
-                line_obj.amount = 0.0
-                formula = line_obj.commission.formula
-                results = line_obj._get_formula_input_dict()
-                safe_eval(formula, results, mode="exec", nocopy=True)
-                line_obj.amount += float(results['result'])
-                # Refunds commissions are negative
-                if line_obj.invoice.type in ('out_refund', 'in_refund'):
-                    line_obj.amount = -line_obj.amount
-            else:
-                return super(AccountInvoiceLineAgent, self)._compute_amount()
+        """Invert sign of refund invoices computed by formula."""
+        super()._compute_amount()
+        applicable_lines = self.filtered(lambda x: (
+            not x.source_product_id.commission_free and x.commission
+            and x.commission.commission_type == 'formula'
+            and x.invoice.type in ('out_refund', 'in_refund')
+        ))
+        for line in applicable_lines:
+            line.amount = -line.amount
