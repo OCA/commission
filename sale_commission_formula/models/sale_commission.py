53f797040d9400fcdf7fb5ffca66126cdb547d3e
Duc Dao <duc.dd@komit-consulting.com>
2019-02-20 19:23:38 +0700

    [MIG] sale_commission_formula: Migration to 12.0

--- sale_commission_formula/models/sale_commission.py
+++ sale_commission_formula/models/sale_commission.py
@@ -2,10 +2,10 @@
 # © 2016 Davide Corio - Abstract
 # License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import models, fields
+from odoo import fields, models
 
 
-class Commission(models.Model):
+class SaleCommission(models.Model):
     _inherit = 'sale.commission'
 
     commission_type = fields.Selection(selection_add=[("formula", "Formula")])
