7f9d3be6aa89082f95de3ae57f7ef04370990923
SimoRubi <simone.rubino@agilebg.com>
2019-03-28 12:13:33 +0100

    [IMP] sale_commission: Add line agents filter for quotation/sale order and invoice

--- sale_commission/__manifest__.py
+++ sale_commission/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Sales commissions',
-    'version': '10.0.2.1.1',
+    'version': '10.0.2.2.0',
     'author': 'Odoo Community Association (OCA),'
               'Tecnativa,'
               'AvanzOSC,'
