1d58ffe5317a1326199db340ac29b079313f6b60
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-03-24 20:31:56 +0100

    [FIX] sale_commission: No commission should be populated on vendor bills

--- sale_commission/__manifest__.py
+++ sale_commission/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2014-2018 Tecnativa - Pedro M. Baeza
 {
     'name': 'Sales commissions',
-    'version': '12.0.1.2.0',
+    'version': '12.0.1.2.1',
     'author': 'Tecnativa,'
               'Odoo Community Association (OCA)',
     'category': 'Sales Management',
