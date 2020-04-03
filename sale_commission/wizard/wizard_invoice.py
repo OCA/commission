a1b5e6b6fb0585cf2a6cf75dc0c53432c6c0f969
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-04-03 20:00:45 +0200

    [MIG+IMP] sale_commission: Switch to computed writable + finish migration + other imps
    * Computed writable saves a lot of code
    * Fix incorrect migration code
    * Fix tests
    * Remove redundant or currently useless tests
    * Adjust multi-company rules
    * Rename fields following guidelines

--- sale_commission/wizard/wizard_invoice.py
+++ sale_commission/wizard/wizard_invoice.py
@@ -5,63 +5,60 @@ class SaleCommissionMakeInvoice(models.TransientModel):
     _name = "sale.commission.make.invoice"
     _description = "Wizard for making an invoice from a settlement"
 
-    def _default_journal(self):
+    def _default_journal_id(self):
         return self.env["account.journal"].search([("type", "=", "purchase")])[:1]
 
-    def _default_refund_journal(self):
-        return self.env["account.journal"].search([("type", "=", "purchase_refund")])[
-            :1
-        ]
-
-    def _default_settlements(self):
+    def _default_settlement_ids(self):
         return self.env.context.get("settlement_ids", [])
 
     def _default_from_settlement(self):
         return bool(self.env.context.get("settlement_ids"))
 
-    journal = fields.Many2one(
+    journal_id = fields.Many2one(
         comodel_name="account.journal",
         required=True,
         domain="[('type', '=', 'purchase')]",
-        default=_default_journal,
+        default=_default_journal_id,
     )
     company_id = fields.Many2one(
-        comodel_name="res.company", related="journal.company_id", readonly=True
+        comodel_name="res.company", related="journal_id.company_id", readonly=True
     )
-    product = fields.Many2one(
+    product_id = fields.Many2one(
         string="Product for invoicing", comodel_name="product.product", required=True
     )
-    settlements = fields.Many2many(
+    settlement_ids = fields.Many2many(
         comodel_name="sale.commission.settlement",
         relation="sale_commission_make_invoice_settlement_rel",
         column1="wizard_id",
         column2="settlement_id",
         domain="[('state', '=', 'settled'),('agent_type', '=', 'agent'),"
         "('company_id', '=', company_id)]",
-        default=_default_settlements,
+        default=_default_settlement_ids,
     )
     from_settlement = fields.Boolean(default=_default_from_settlement)
     date = fields.Date(default=fields.Date.context_today)
 
     def button_create(self):
         self.ensure_one()
-        if not self.settlements:
-            self.settlements = self.env["sale.commission.settlement"].search(
+        if self.settlement_ids:
+            settlements = self.settlement_ids
+        else:
+            settlements = self.env["sale.commission.settlement"].search(
                 [
                     ("state", "=", "settled"),
                     ("agent_type", "=", "agent"),
-                    ("company_id", "=", self.journal.company_id.id),
+                    ("company_id", "=", self.journal_id.company_id.id),
                 ]
             )
-        self.settlements.make_invoices(self.journal, self.product, date=self.date)
+        invoices = settlements.make_invoices(
+            self.journal_id, self.product_id, date=self.date
+        )
         # go to results
-        if len(self.settlements):
+        if len(settlements):
             return {
                 "name": _("Created Invoices"),
                 "type": "ir.actions.act_window",
                 "views": [[False, "list"], [False, "form"]],
                 "res_model": "account.move",
-                "domain": [["id", "in", [x.invoice.id for x in self.settlements]]],
+                "domain": [["id", "in", invoices.ids]],
             }
-        else:
-            return {"type": "ir.actions.act_window_close"}
