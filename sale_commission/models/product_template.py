17ca88dbd939a57b11700aee099341772e49a47c
Manuel Calero <manuel.calero@tecnativa.com>
2020-05-11 19:57:08 +0200

    [IMP] : black, isort, prettier

--- sale_commission/models/product_template.py
+++ sale_commission/models/product_template.py
@@ -1,9 +1,7 @@
-
 from odoo import fields, models
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
-    commission_free = fields.Boolean(string="Free of commission",
-                                     default=False)
+    commission_free = fields.Boolean(string="Free of commission", default=False)
