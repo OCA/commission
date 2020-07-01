d05776d75e8dc88716e6165f14f329fe5390f8bc
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-07-01 17:04:29 +0200

    [IMP] sale_commission: Settlement multi-company rule
    Another one for having proper multi-company support.

--- sale_commission/__manifest__.py
+++ sale_commission/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2014-2020 Tecnativa - Pedro M. Baeza
 {
     'name': 'Sales commissions',
-    'version': '12.0.1.3.0',
+    'version': '12.0.1.4.0',
     'author': 'Tecnativa,'
               'Odoo Community Association (OCA)',
     'category': 'Sales Management',
