152335f6bd23579ec13e64178a5520ce66f2823e
Mihai Fekete <feketemihai@odooerpromania.ro>
2020-07-10 20:20:58 +0300

    [MIG] sale_commission_formula: Migration to 13.0

--- sale_commission_formula/models/__init__.py
+++ sale_commission_formula/models/__init__.py
@@ -1,4 +1,2 @@
-# License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
-
 from . import sale_commission
 from . import sale_commission_mixin
--- sale_commission_formula/tests/test_commission_formula.py
+++ sale_commission_formula/tests/test_commission_formula.py
@@ -8,12 +8,23 @@ from odoo.tests.common import TransactionCase
 class TestCommissionFormula(TransactionCase):
     def setUp(self):
         super(TestCommissionFormula, self).setUp()
+        self.advance_inv_model = self.env["sale.advance.payment.inv"]
         self.agent = self.env.ref("sale_commission_formula.agent1")
         self.commission = self.env.ref(
             "sale_commission_formula.commission_5perc10extra"
         )
         self.sale_order = self.env.ref("sale_commission_formula.sale_order_1")
         self.so_line = self.env.ref("sale_commission_formula.sale_order_line_1")
+
+    def _invoice_sale_order(self, sale_order):
+        wizard = self.advance_inv_model.create({"advance_payment_method": "delivered"})
+        wizard.with_context(
+            {
+                "active_model": "sale.order",
+                "active_ids": [sale_order.id],
+                "active_id": sale_order.id,
+            }
+        ).create_invoices()
 
     def test_sale_order_commission(self):
         # we test the '5% + 10% extra' we should return 41.25 since
@@ -22,8 +33,8 @@ class TestCommissionFormula(TransactionCase):
         self.env["sale.order.line.agent"].create(
             {
                 "object_id": self.so_line.id,
-                "agent": self.agent.id,
-                "commission": self.commission.id,
+                "agent_id": self.agent.id,
+                "commission_id": self.commission.id,
             }
         )
         self.assertEqual(41.25, self.sale_order.commission_total)
@@ -32,16 +43,17 @@ class TestCommissionFormula(TransactionCase):
         # we confirm the sale order and create the corresponding invoice
         self.sale_order.action_confirm()
         self.so_line.qty_delivered = self.so_line.product_uom_qty
-        invoice_id = self.sale_order.action_invoice_create()
-        invoice = self.env["account.invoice"].browse(invoice_id)
+        self._invoice_sale_order(self.sale_order)
+        self.sale_order.invoice_ids.post()
+        invoice = self.sale_order.invoice_ids[0]
         # we add the commissions on the first invoice line
         invoice_line = invoice.invoice_line_ids[0]
         invoice_line.agents = False  # Erase current agents
         self.env["account.invoice.line.agent"].create(
             {
                 "object_id": invoice_line.id,
-                "agent": self.agent.id,
-                "commission": self.commission.id,
+                "agent_id": self.agent.id,
+                "commission_id": self.commission.id,
             }
         )
         # we test the '5% + 10% extra' commissions on the invoice too
@@ -51,22 +63,21 @@ class TestCommissionFormula(TransactionCase):
         # we confirm the sale order and create the corresponding invoice
         self.sale_order.action_confirm()
         self.so_line.qty_delivered = self.so_line.product_uom_qty
-        invoice_id = self.sale_order.action_invoice_create()
-        invoice = self.env["account.invoice"].browse(invoice_id)
+        self._invoice_sale_order(self.sale_order)
+        self.sale_order.invoice_ids.post()
+        invoice = self.sale_order.invoice_ids[0]
         # we add the commissions on the first invoice line
         invoice_line = invoice.invoice_line_ids[0]
         invoice_line.agents = False  # Erase current agents
         self.env["account.invoice.line.agent"].create(
             {
                 "object_id": invoice_line.id,
-                "agent": self.agent.id,
-                "commission": self.commission.id,
+                "agent_id": self.agent.id,
+                "commission_id": self.commission.id,
             }
         )
         # we test the '5% + 10% extra' commissions on the invoice too
         self.assertEqual(41.25, invoice.commission_total)
-        invoice.action_invoice_open()
-        self.env["account.invoice.refund"].create(
-            {"filter_refund": "cancel", "description": "Wrong product"}
-        ).with_context(active_ids=invoice.id).invoice_refund()
-        self.assertEqual(-41.25, invoice.refund_invoice_ids[0].commission_total)
+        invoice.post()
+        reverse = invoice._reverse_moves(cancel=True)
+        self.assertEqual(-41.25, reverse.commission_total)
