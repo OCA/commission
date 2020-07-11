032ff72d323bef461bb8b9fc3a6b4c29a5d7fb8a
Mihai Fekete <feketemihai@odooerpromania.ro>
2020-07-11 11:38:10 +0300

    [IMP] sale_commission_pricelist: black, isort, prettier

--- sale_commission_pricelist/models/sale_order.py
+++ sale_commission_pricelist/models/sale_order.py
@@ -6,28 +6,27 @@ from odoo import api, models
 
 
 class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
     def _get_commission_from_pricelist(self):
         self.ensure_one()
         if not self.product_id or not self.order_id.pricelist_id:
-            return False    # pragma: no cover
+            return False  # pragma: no cover
         rule_id = self.order_id.pricelist_id.get_product_price_rule(
             product=self.product_id,
             quantity=self.product_uom_qty or 1.0,
             partner=self.order_id.partner_id,
             date=self.order_id.date_order,
-            uom_id=self.product_uom.id)[1]
-        rule = self.env['product.pricelist.item'].browse(rule_id)
+            uom_id=self.product_uom.id,
+        )[1]
+        rule = self.env["product.pricelist.item"].browse(rule_id)
         return rule.commission_id
 
-    @api.onchange('product_id', 'product_uom_qty')
+    @api.onchange("product_id", "product_uom_qty")
     def _onchange_product_id_sale_commission_pricelist(self):
         commission = self._get_commission_from_pricelist()
         if commission:
-            self.agents.update({
-                'commission': commission.id,
-            })
+            self.agents.update({"commission": commission.id})
 
     def _prepare_agents_vals(self):
         self.ensure_one()
@@ -35,5 +34,5 @@ class SaleOrderLine(models.Model):
         commission = self._get_commission_from_pricelist()
         if commission:
             for vals in res:
-                vals[2]['commission'] = commission.id
+                vals[2]["commission"] = commission.id
         return res
