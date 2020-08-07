e2e2c0dd3c9d48c9d25e0f44888bc563266d5d60
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-08-07 20:09:43 +0200

    [MIG] sale_commission_salesman: Migration to 13.0
    * Standard procedure
    * Adapt account.invoice > account.move
    * Adapt tests

--- sale_commission_salesman/tests/test_sale_commission_salesman.py
+++ sale_commission_salesman/tests/test_sale_commission_salesman.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import exceptions
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import Form, SavepointCase
 
 
 class TestSaleCommissionSalesman(SavepointCase):
@@ -10,93 +10,79 @@ class TestSaleCommissionSalesman(SavepointCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.product = cls.env["product.product"].create(
-            {"name": "Test Product 1", "list_price": 100,}
+            {"name": "Test Product 1", "list_price": 100}
         )
         SaleCommission = cls.env["sale.commission"]
         cls.commission_1 = SaleCommission.create(
-            {"name": "1% commission", "fix_qty": 1.0,}
+            {"name": "1% commission", "fix_qty": 1.0}
         )
         Partner = cls.env["res.partner"]
         cls.salesman = cls.env["res.users"].create(
-            {"name": "Test agent", "login": "sale_comission_salesman_test",}
+            {"name": "Test agent", "login": "sale_comission_salesman_test"}
         )
         cls.agent = cls.salesman.partner_id
         cls.agent.write(
             {
                 "agent": True,
                 "salesman_as_agent": True,
-                "commission": cls.commission_1.id,
+                "commission_id": cls.commission_1.id,
             }
         )
         cls.other_agent = Partner.create(
             {
                 "name": "Test other agent",
                 "agent": True,
-                "commission": cls.commission_1.id,
+                "commission_id": cls.commission_1.id,
             }
         )
-        cls.partner = Partner.create({"name": "Partner test",})
+        cls.partner = Partner.create({"name": "Partner test"})
         cls.sale_order = cls.env["sale.order"].create(
-            {"partner_id": cls.partner.id, "user_id": cls.salesman.id,}
+            {"partner_id": cls.partner.id, "user_id": cls.salesman.id}
         )
-        cls.invoice = cls.env["account.invoice"].create(
-            {"partner_id": cls.partner.id, "user_id": cls.salesman.id,}
+        cls.invoice = cls.env["account.move"].create(
+            {
+                "partner_id": cls.partner.id,
+                "invoice_user_id": cls.salesman.id,
+                "type": "out_invoice",
+            }
         )
 
     def test_check_salesman_commission(self):
         with self.assertRaises(exceptions.ValidationError):
-            self.agent.commission = False
+            self.agent.commission_id = False
 
     def test_sale_commission_salesman(self):
-        line = (
-            self.env["sale.order.line"]
-            .with_context(partner_id=self.partner.id)
-            .create({"order_id": self.sale_order.id, "product_id": self.product.id,})
+        line = self.env["sale.order.line"].create(
+            {"order_id": self.sale_order.id, "product_id": self.product.id}
         )
-        self.assertTrue(line.agents)
-        self.assertTrue(line.agents.agent, self.agent)
-        self.assertTrue(line.agents.commission, self.commission_1)
+        self.assertTrue(line.agent_ids)
+        self.assertTrue(line.agent_ids.agent_id, self.agent)
+        self.assertTrue(line.agent_ids.commission_id, self.commission_1)
 
     def test_sale_commission_salesman_no_population(self):
-        self.partner.agents = [(4, self.other_agent.id)]
-        line = (
-            self.env["sale.order.line"]
-            .with_context(partner_id=self.partner.id)
-            .create({"order_id": self.sale_order.id, "product_id": self.product.id,})
+        self.partner.agent_ids = [(4, self.other_agent.id)]
+        line = self.env["sale.order.line"].create(
+            {"order_id": self.sale_order.id, "product_id": self.product.id}
         )
-        self.assertTrue(len(line.agents), 1)
-        self.assertTrue(line.agents.agent, self.other_agent)
+        self.assertTrue(len(line.agent_ids), 1)
+        self.assertTrue(line.agent_ids.agent_id, self.other_agent)
 
     def test_invoice_commission_salesman(self):
-        line_obj = self.env["account.invoice.line"]
-        line = line_obj.with_context(partner_id=self.partner.id).create(
-            {
-                "invoice_id": self.invoice.id,
-                "product_id": self.product.id,
-                "account_id": line_obj.get_invoice_line_account(
-                    self.invoice.type, self.product, False, self.invoice.company_id
-                ).id,
-                "name": self.product.name,
-                "price_unit": 1,
-            }
-        )
-        self.assertTrue(line.agents)
-        self.assertTrue(line.agents.agent, self.agent)
-        self.assertTrue(line.agents.commission, self.commission_1)
+        invoice_form = Form(self.invoice)
+        with invoice_form.invoice_line_ids.new() as line_form:
+            line_form.product_id = self.product
+        invoice_form.save()
+        line = self.invoice.invoice_line_ids
+        self.assertTrue(line.agent_ids)
+        self.assertTrue(line.agent_ids.agent_id, self.agent)
+        self.assertTrue(line.agent_ids.commission_id, self.commission_1)
 
     def test_invoice_commission_salesman_no_population(self):
-        self.partner.agents = [(4, self.other_agent.id)]
-        line_obj = self.env["account.invoice.line"]
-        line = line_obj.with_context(partner_id=self.partner.id).create(
-            {
-                "invoice_id": self.invoice.id,
-                "product_id": self.product.id,
-                "account_id": line_obj.get_invoice_line_account(
-                    self.invoice.type, self.product, False, self.invoice.company_id
-                ).id,
-                "name": self.product.name,
-                "price_unit": 1,
-            }
-        )
-        self.assertTrue(len(line.agents), 1)
-        self.assertTrue(line.agents.agent, self.other_agent)
+        self.partner.agent_ids = [(4, self.other_agent.id)]
+        invoice_form = Form(self.invoice)
+        with invoice_form.invoice_line_ids.new() as line_form:
+            line_form.product_id = self.product
+        invoice_form.save()
+        line = self.invoice.invoice_line_ids
+        self.assertTrue(line.agent_ids)
+        self.assertTrue(line.agent_ids.agent_id, self.other_agent)
