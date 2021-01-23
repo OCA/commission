e611e8078043bded9bb893a14434fc9c90505a5f
Andrea Cometa <a.cometa@apuliasoftware.it>
2021-01-23 11:55:22 +0100

    add view on agent showing own products and categories custom commissions

--- sale_commission_product/__manifest__.py
+++ sale_commission_product/__manifest__.py
@@ -16,6 +16,7 @@
     "data": [
         "security/ir.model.access.csv",
         "views/product_view.xml",
+        "views/partner_view.xml",
     ],
     "installable": True,
 }
--- sale_commission_product/models/__init__.py
+++ sale_commission_product/models/__init__.py
@@ -5,3 +5,4 @@
 from . import product
 from . import sale_order
 from . import account_invoice
+from . import res_partner
