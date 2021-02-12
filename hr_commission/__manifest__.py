d32cfeef79d60522281206a54f638dd1234a6e04
João Marques <joao.marques@tecnativa.com>
2021-02-12 10:16:55 +0000

    [MIG] hr_commission: Migration to 14.0
    TT26922

--- hr_commission/__manifest__.py
+++ hr_commission/__manifest__.py
@@ -1,10 +1,12 @@
 # Copyright 2015-2020 Tecnativa - Pedro M. Baeza
+# Copyright 2021 Tecnativa - João Marques
 # License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
 
 {
     "name": "HR commissions",
-    "version": "12.0.1.1.0",
+    "version": "13.0.1.0.0",
     "author": "Tecnativa," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/commission",
     "category": "Commissions",
     "depends": ["sale_commission", "hr"],
     "license": "AGPL-3",
--- hr_commission/readme/CONTRIBUTORS.rst
+++ hr_commission/readme/CONTRIBUTORS.rst
@@ -1,5 +1,5 @@
 * `Tecnativa <https://www.tecnativa.com>`_
 
   * Pedro M. Baeza
-
+  * João Marques
 * Rattapong C. <rattapongc@ecosoft.co.th>
