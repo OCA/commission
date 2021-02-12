fd33edb8a51a1470525648d7665f10ba6a7e0374
João Marques <joao.marques@tecnativa.com>
2021-02-12 10:16:55 +0000

    [MIG] hr_commission: Migration to 14.0
    TT26922

--- hr_commission/models/res_partner.py
+++ hr_commission/models/res_partner.py
@@ -1,4 +1,5 @@
 # Copyright 2015-2018 Tecnativa - Pedro M. Baeza
+# Copyright 2021 Tecnativa - Jo??o Marques
 # License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import _, api, exceptions, fields, models
@@ -9,7 +10,7 @@ class ResPartner(models.Model):
 
     agent_type = fields.Selection(selection_add=[("salesman", "Salesman (employee)")],)
     employee_id = fields.Many2one(
-        string="Employee.",
+        string="Related Employee",
         comodel_name="hr.employee",
         compute="_compute_employee_id",
         compute_sudo=True,
@@ -18,11 +19,10 @@ class ResPartner(models.Model):
     @api.depends("user_ids")
     def _compute_employee_id(self):
         for partner in self:
-            if (
-                len(partner.user_ids) == 1
-                and len(partner.user_ids[0].employee_ids) == 1
-            ):
+            if len(partner.user_ids) == 1 and partner.user_ids[0].employee_ids:
                 partner.employee_id = partner.user_ids[0].employee_ids[0]
+            else:
+                partner.employee_id = False
 
     @api.constrains("agent_type")
     def _check_employee(self):
@@ -30,8 +30,8 @@ class ResPartner(models.Model):
             raise exceptions.ValidationError(
                 _(
                     "There must one (and only one) employee linked to this "
-                    "partner. To do this, go to 'Human Resources' and check "
-                    "'Employees'"
+                    "partner. To do this, go to 'Employees' and create an "
+                    "Employee with a 'Related User' under 'HR Settings'."
                 )
             )
 
