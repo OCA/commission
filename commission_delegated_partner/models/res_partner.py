795a043877a5d314ab23ef5bc632b383c15bb331
Olga Marco <olga.marco@creublanca.es>
2021-07-12 12:56:47 +0200

    [IMP] sale_commission_delegated_partner: black, isort, prettier

--- commission_delegated_partner/models/res_partner.py
+++ commission_delegated_partner/models/res_partner.py
@@ -6,8 +6,6 @@ from odoo import fields, models
 
 class ResPartner(models.Model):
 
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
-    delegated_agent_id = fields.Many2one(
-        "res.partner"
-    )
+    delegated_agent_id = fields.Many2one("res.partner")
