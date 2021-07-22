6de64d8aac48e9d84e00d5676e95d039ff4c446f
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2021-07-22 10:04:55 -0500

    [FIX] sale_commission

--- sale_commission/tests/test_sale_commission.py
+++ sale_commission/tests/test_sale_commission.py
@@ -197,7 +197,7 @@ class TestSaleCommission(SavepointCase):
             .create({"journal_id": journal.id})
         )
         register_payments.action_create_payments()
-        self.assertEqual(sale_order.invoice_ids.payment_state, "paid")
+        self.assertIn(sale_order.invoice_ids.payment_state, ["in_payment", "paid"])
         self._settle_agent(agent, period)
         settlements = self.settle_model.search([("state", "=", "settled")])
         self.assertTrue(settlements)
