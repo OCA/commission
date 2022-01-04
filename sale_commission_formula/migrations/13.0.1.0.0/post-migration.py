c29c19d1a573efae1cb01be51df74e5336d916df
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-01-04 20:58:43 +0100

    [OU-FIX] sale_commission_formula: Replace formulas also on inactive records
    TT33764

--- sale_commission_formula/migrations/13.0.1.0.0/post-migration.py
+++ sale_commission_formula/migrations/13.0.1.0.0/post-migration.py
@@ -1,11 +1,16 @@
 # Copyright 2020 NextERP Romania SRL
+# Copyright 2021 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from openupgradelib import openupgrade
 
 
 @openupgrade.migrate()
 def migrate(env, version):
-    formula_comm = env["sale.commission"].search([("commission_type", "=", "formula")])
+    formula_comm = (
+        env["sale.commission"]
+        .with_context(active_test=False)
+        .search([("commission_type", "=", "formula")])
+    )
     for com in formula_comm:
         if "account.invoice.line" in com.formula:
             com.formula = com.formula.replace(
