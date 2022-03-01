b2896c6b06c6f62d0ccd27f39eddd806cd23d598
Ilyas <yjm@mail.ru>
2022-03-01 01:34:35 -0800

    [UPD] add new commission type - by category product or variant

--- sale_commission/models/sale_order.py
+++ sale_commission/models/sale_order.py
@@ -56,6 +56,14 @@ class SaleOrderLineAgent(models.Model):
 
     object_id = fields.Many2one(comodel_name="sale.order.line")
     currency_id = fields.Many2one(related="object_id.currency_id")
+    discount = fields.Float(related="object_id.discount")
+    applied_commission_item_id = fields.Many2one("commission.item")
+    applied_on_name = fields.Char(related="applied_commission_item_id.name")
+    commission_type = fields.Selection(
+        related="applied_commission_item_id.commission_type"
+    )
+    fixed_amount = fields.Float(related="applied_commission_item_id.fixed_amount")
+    percent_amount = fields.Float(related="applied_commission_item_id.percent_amount")
 
     @api.depends(
         "object_id.price_subtotal", "object_id.product_id", "object_id.product_uom_qty"
