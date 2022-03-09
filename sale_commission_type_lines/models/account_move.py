9cd8a81e4b8978f9f6315a861faf509a88311272
Ilyas <irazor147@gmail.com>
2022-03-09 01:40:05 -0800

    [FIX] compute commission for down payment invoice

--- sale_commission_type_lines/models/account_move.py
+++ sale_commission_type_lines/models/account_move.py
@@ -5,3 +5,12 @@ class AccountInvoiceLineAgent(models.Model):
     _inherit = "account.invoice.line.agent"
 
     applied_commission_item_id = fields.Many2one("commission.item")
+
+    def _get_commission_amount(self, commission, subtotal, product, quantity):
+        so = self.env['sale.order'].search([('invoice_ids', 'in', self.invoice_id.ids)])
+        if so:
+            # required when using down payment invoice (fixed or percent).
+            commission_part = self.invoice_id.amount_total / so.amount_total
+            invoice_commission = commission_part * so.commission_total
+            return invoice_commission
+        return 0.0
