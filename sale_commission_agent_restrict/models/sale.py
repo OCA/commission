075006201d9786297cf1e3146a8e61343ad120a7
Ilyas <irazor147@gmail.com>
2022-08-29 18:59:52 +0500

    [14.0][IMP] sale_commission_agent_restrict: hide customers contacts (do not populate agent_ids among child ids of the customer)

--- sale_commission_agent_restrict/models/sale.py
+++ sale_commission_agent_restrict/models/sale.py
@@ -1,3 +1,5 @@
+import json
+
 from odoo import api, fields, models
 
 
@@ -9,6 +11,7 @@ class SaleOrder(models.Model):
         compute="_compute_commission_total",
         store=True,
     )
+    partner_id_domain = fields.Char(compute="_compute_partner_id_domain")
 
     @api.depends("order_line.agent_ids.amount")
     def _compute_commission_total(self):
@@ -18,3 +21,16 @@ class SaleOrder(models.Model):
                 lambda x: x.agent_id == self.env.user.partner_id
             )
             record.current_agent_total = sum(my_com.mapped("amount"))
+
+    @api.depends_context("company", "uid")
+    def _compute_partner_id_domain(self):
+        for rec in self:
+            rec.partner_id_domain = json.dumps(
+                [
+                    "&",
+                    ("id", "!=", self.env.user.partner_id.id),
+                    "|",
+                    ("company_id", "=", False),
+                    ("company_id", "=", self.env.company.id),
+                ]
+            )
