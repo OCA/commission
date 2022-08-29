4a043975abe866f614a2dcb9c325473f6e5c6c46
Ilyas <irazor147@gmail.com>
2022-08-29 15:11:47 +0500

    [14.0][ADD] sale_commission_queued

---
+++ sale_commission_queued/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Sales commissions queued",
+    "version": "14.0.1.0.0",
+    "author": "Ilyas, Ooops404," "Odoo Community Association (OCA)",
+    "category": "Sales Management",
+    "license": "AGPL-3",
+    "depends": ["sale_commission", "queue_job"],
+    "website": "https://github.com/OCA/commission",
+    "maintainers": ["pedrobaeza"],
+    "data": [
+        "wizard/wizard_invoice.xml",
+    ],
+    "installable": True,
+}
