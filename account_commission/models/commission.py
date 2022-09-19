05ec496a4e461a6e9e35a43ce4b9118eb1d1c1f4
Aungkokolin1997 <aungkokolin23397@gmail.com>
2022-09-19 23:11:50 +0630

    [MIG] sale_commission: Migration to 15.0

---
+++ account_commission/models/commission.py
@@ -0,0 +1,14 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class Commission(models.Model):
+    _inherit = "commission"
+
+    invoice_state = fields.Selection(
+        [("open", "Invoice Based"), ("paid", "Payment Based")],
+        string="Invoice Status",
+        required=True,
+        default="open",
+    )
---
+++ commission/readme/ROADMAP.rst
@@ -0,0 +1,5 @@
+* Make it totally multi-company aware.
+* Be multi-currency aware for settlements.
+* Allow to calculate and pay in other currency different from company one.
+* Set agent popup window with a kanban view with richer information and
+  mobile friendly.
