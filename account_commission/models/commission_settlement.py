589877d6ff66b2612ce2d41f29657865cd48cfb4
Aungkokolin1997 <aungkokolin23397@gmail.com>
2022-12-07 13:03:31 +0630

    [IMP] account_commission: Add link invoice > settlement

--- account_commission/models/commission_settlement.py
+++ account_commission/models/commission_settlement.py
@@ -30,6 +30,7 @@ class CommissionSettlement(models.Model):
         readonly=True,
     )
     invoice_id = fields.Many2one(
+        string="Generated Invoice",
         store=True,
         comodel_name="account.move",
         compute="_compute_invoice_id",
@@ -44,7 +45,9 @@ class CommissionSettlement(models.Model):
     @api.depends("invoice_line_ids")
     def _compute_invoice_id(self):
         for record in self:
-            record.invoice_id = record.invoice_line_ids[:1].move_id
+            record.invoice_id = record.invoice_line_ids.filtered(
+                lambda x: x.parent_state != "cancel"
+            )[:1].move_id
 
     def action_cancel(self):
         """Check if any settlement has been invoiced."""
