06d324064ad097d1124c775b0e2d05b1af6b8deb
Luis Ernesto García Medina <ernesto.garcia@tecnativa.com>
2023-02-23 17:40:35 -0600

    [MIG] sale_commission_salesman: Migration to 15.0

--- sale_commission_salesman/models/sale_order.py
+++ sale_commission_salesman/models/sale_order.py
@@ -11,8 +11,9 @@ class SaleOrdeLine(models.Model):
         """Add salesman agent if configured so and no other commission
         already populated.
         """
-        super()._compute_agent_ids()
+        result = super()._compute_agent_ids()
         for record in self.filtered(lambda x: x.order_id.partner_id):
             partner = record.order_id.user_id.partner_id
             if not record.agent_ids and partner.agent and partner.salesman_as_agent:
                 record.agent_ids = [(0, 0, record._prepare_agent_vals(partner))]
+        return result
