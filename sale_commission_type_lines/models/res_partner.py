a15701f3be281c950dc6b449d64634e683047f19
Ilyas <irazor147@gmail.com>
2023-04-14 14:43:12 +0200

    [14.0][IMP]

--- sale_commission_type_lines/models/res_partner.py
+++ sale_commission_type_lines/models/res_partner.py
@@ -1,7 +1,6 @@
 # © 2023 ooops404
 # License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
-from odoo import _, api, fields, models
-from odoo.exceptions import ValidationError
+from odoo import models
 
 
 class ResPartner(models.Model):
