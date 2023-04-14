7c7c11f7cd25b91f8814bb6c4d1c345eb64d8cd2
Ilyas <irazor147@gmail.com>
2023-04-14 15:27:46 +0200

    [14.0][IMP]

--- sale_commission_type_lines/models/__init__.py
+++ sale_commission_type_lines/models/__init__.py
@@ -1,7 +1,6 @@
 from . import commission
 from . import sale
 from . import account_move
-from . import res_partner
 from . import settlement
 from . import res_config_settings
 from . import res_company
--- sale_commission_type_lines/models/res_partner.py
+++
@@ -1,7 +0,0 @@
-# © 2023 ooops404
-# License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
-from odoo import models
-
-
-class ResPartner(models.Model):
-    _inherit = "res.partner"
