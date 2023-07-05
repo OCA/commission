c7261899c74b09ee6b4adf8330dbd996ea6226d3
Ilyas <irazor147@gmail.com>
2023-07-05 11:03:58 +0200

    [IMP] sale_commission_product_criteria: added commission items search fields

--- sale_commission_product_criteria/__manifest__.py
+++ sale_commission_product_criteria/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Sale Commission Product Criteria",
     "summary": "Advanced commissions rules",
-    "version": "14.0.1.0.2",
+    "version": "14.0.1.0.3",
     "author": "Ilyas, Ooops404, Odoo Community Association (OCA)",
     "maintainers": ["ilyasProgrammer"],
     "website": "https://github.com/OCA/commission",
