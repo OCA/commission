2619d22225ded40d65fdd824d622eb7bb92e5c11
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2023-08-10 12:25:37 +0200

    [OU-FIX] commission: Field commission.settlement.line~commission_id

--- commission/migrations/15.0.2.0.0/pre-migration.py
+++ commission/migrations/15.0.2.0.0/pre-migration.py
@@ -1,4 +1,5 @@
 # Copyright 2023 Tecnativa - David Vidal
+# Copyright 2023 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from openupgradelib import openupgrade
 
@@ -20,7 +21,29 @@ model_renames = [
 ]
 
 
+def _handle_settlement_line_commission_id(env):
+    """On the new version, this field is computed stored, but the compute method
+    doesn't resolve correctly the link here (as it's handled in `account_commission`),
+    so we pre-create the column and fill it properly according old expected data.
+    """
+    openupgrade.logged_query(
+        env.cr, "ALTER TABLE commission_settlement_line ADD commission_id int4"
+    )
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE commission_settlement_line csl
+        SET commission_id = aila.commission_id
+        FROM settlement_agent_line_rel rel
+        JOIN account_invoice_line_agent aila ON aila.id = rel.agent_line_id
+        WHERE rel.settlement_id = csl.id
+        AND csl.commission_id IS NULL
+        """,
+    )
+
+
 @openupgrade.migrate(no_version=True)
 def migrate(env, version):
     openupgrade.rename_tables(env.cr, table_renames)
     openupgrade.rename_models(env.cr, model_renames)
+    _handle_settlement_line_commission_id(env)
