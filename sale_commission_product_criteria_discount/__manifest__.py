1fa64a1810a0c7670244b2ca381998536815eddd
Ilyas <irazor147@gmail.com>
2023-08-22 16:26:30 +0700

    [IMP] sale_commission_product_criteria_discount: correct commission type check

--- sale_commission_product_criteria_discount/__manifest__.py
+++ sale_commission_product_criteria_discount/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Sale Commission Product Criteria Discount",
     "summary": "Advanced commissions rules with discount",
-    "version": "14.0.1.0.1",
+    "version": "14.0.1.0.2",
     "author": "Ilyas," "Ooops404," "Odoo Community Association (OCA)",
     "contributors": ["Ilyas"],
     "maintainers": ["ilyasProgrammer"],
