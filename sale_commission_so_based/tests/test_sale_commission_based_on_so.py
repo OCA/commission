e2e45a12c9017573ca9cce1fe9c0ed92b2ab55b8
maksym <maximkv25@gmail.com>
2023-10-24 15:10:27 +0300

    [IMP] increase test coverage

--- sale_commission_so_based/tests/test_sale_commission_based_on_so.py
+++ sale_commission_so_based/tests/test_sale_commission_based_on_so.py
@@ -1,10 +1,12 @@
 # Copyright 2016-2022 Tecnativa - Pedro M. Baeza
-from odoo.tests import tagged
+from odoo import fields
+from odoo.tests import Form, tagged
 from odoo.addons.sale_commission.tests.test_sale_commission import TestSaleCommission
 
 
 @tagged("post_install", "-at_install")
 class TestSaleCommissionBasedOnSO(TestSaleCommission):
+
     def _settle_agent_sale_order(self, agent=None, period=None, date=None):
         vals = self._get_make_settle_vals(agent, period, date)
         vals["settlement_type"] = "sale_order"
@@ -31,4 +33,10 @@ class TestSaleCommissionBasedOnSO(TestSaleCommission):
 
         settlement = self.settle_model.search([("state", "=", "settled")])
         self.assertEqual(len(settlement), 1)
-        self.assertEqual(settlement.settlement_type, "sale_order")
+        self.assertEqual(settlement.settlement_type, 'sale_order')
+
+        self.assertTrue(sale_order.order_line.agent_ids.settled)
+
+        self.assertEqual(fields.Date.today(), settlement.line_ids.date)
+        self.assertTrue(settlement.line_ids.commission_id)
+        self.assertEqual(settlement.line_ids.settled_amount, 0)
