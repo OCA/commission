5e04b2e659596df26c9639a75aad427d45b8b392
ambientWave <ambientWave@online.dev>
2024-01-19 16:28:32 +0400

    [MIG] sale_commission: Migration to 17.0

--- sale_commission/__manifest__.py
+++ sale_commission/__manifest__.py
@@ -3,7 +3,7 @@
 # Copyright 2014-2022 Tecnativa - Pedro M. Baeza
 {
     "name": "Sales commissions",
-    "version": "16.0.1.0.1",
+    "version": "17.0.1.0.0",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "category": "Sales Management",
     "license": "AGPL-3",
