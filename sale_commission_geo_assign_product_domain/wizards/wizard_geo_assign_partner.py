e070dd37ec26c63b58fa3c29c0427d95eed57604
PicchiSeba <picchiseba+git@gmail.com>
2024-07-05 15:49:42 +0200

    [IMP]sale_commission_geo_assign_product_domain: performance improvements

--- sale_commission_geo_assign_product_domain/wizards/wizard_geo_assign_partner.py
+++ sale_commission_geo_assign_product_domain/wizards/wizard_geo_assign_partner.py
@@ -8,35 +8,39 @@ class WizardGeoAssign(models.TransientModel):
     _inherit = "wizard.geo.assign.partner"
 
     @api.model
-    def update_partner_data(self, partner, agent):
-        if agent.commission_type == "product_restricted":
+    def update_partner_data(self, partner, agents):
+        product_restricted_agents = agents.filtered(
+            lambda a: a.commission_type == "product_restricted"
+        )
+        partner_agent_ids = self.env["res.partner"]
+        for agent in product_restricted_agents:
             for line in agent.commission_geo_group_ids:
-                if line.is_assignable(partner):
-                    partner.agent_ids = [(4, agent.id)]
-
-                    commission_item = partner.commission_item_agent_ids.filtered(
-                        lambda x: x.agent_id == agent
-                    )
-                    if commission_item:
-                        commission_item.group_ids = [
-                            (4, x) for x in line.mapped("commission_group_ids.id")
-                        ]
-                    else:
-                        partner.commission_item_agent_ids = [
-                            (
-                                0,
-                                0,
-                                {
-                                    "agent_id": agent.id,
-                                    "group_ids": [
-                                        (
-                                            6,
-                                            0,
-                                            line.mapped("commission_group_ids.id"),
-                                        )
-                                    ],
-                                },
-                            )
-                        ]
-        else:
-            super().update_partner_data(partner, agent)
+                if not line.is_assignable(partner):
+                    continue
+                partner_agent_ids += agent
+                commission_item = partner.commission_item_agent_ids.filtered(
+                    lambda x: x.agent_id == agent
+                )
+                if commission_item:
+                    commission_item.group_ids = [
+                        (4, x) for x in line.mapped("commission_group_ids.id")
+                    ]
+                else:
+                    partner.commission_item_agent_ids = [
+                        (
+                            0,
+                            0,
+                            {
+                                "agent_id": agent.id,
+                                "group_ids": [
+                                    (
+                                        6,
+                                        0,
+                                        line.mapped("commission_group_ids.id"),
+                                    )
+                                ],
+                            },
+                        )
+                    ]
+        partner.agent_ids += partner_agent_ids
+        super().update_partner_data(partner, agents - product_restricted_agents)
