f9545f9e43c8430b9dacb8309cf12f48719ef026
stenex <stefano.nana@nextev.it>
2024-07-17 14:28:14 +0200

    [IMP] sale_commission_product: pre-commit stuff

--- sale_commission_product/__openerp__.py
+++ sale_commission_product/__openerp__.py
@@ -1,19 +1,17 @@
-# -*- coding: utf-8 -*-
 # © 2015 Alejandro Sánchez Ramírez (<http://www.asr-oss.com>)
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    'name': 'Sale commissions product',
-    'version': '8.0.1.0.1',
-    'author': 'Asr Oss - Alejandro Sánchez, '
-              'Odoo Community Association (OCA)',
+    "name": "Sale commissions product",
+    "version": "8.0.1.0.1",
+    "author": "Asr Oss - Alejandro Sánchez, " "Odoo Community Association (OCA)",
     "category": "Generic Modules/Sales & Purchases",
-    'license': 'AGPL-3',
-    'depends': [
-        'sale_commission',
-        'web_widget_one2many_tags',
+    "license": "AGPL-3",
+    "depends": [
+        "sale_commission",
+        "web_widget_one2many_tags",
     ],
-    'contributors': [
+    "contributors": [
         "Alejandro Sánchez <alejandro@asr-oss.com>",
     ],
     "data": [
--- sale_commission_product/models/product.py
+++ sale_commission_product/models/product.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2015 Alejandro Sánchez Ramírez (<http://www.asr-oss.com>)
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
@@ -6,40 +5,47 @@ from openerp import api, fields, models
 
 
 class ProductProduct(models.Model):
-    _inherit = 'product.product'
+    _inherit = "product.product"
 
     agents = fields.One2many(
         string="Agents & commissions",
-        comodel_name='product.product.agent', inverse_name='product_id',
-        copy=True, readonly=True)
+        comodel_name="product.product.agent",
+        inverse_name="product_id",
+        copy=True,
+        readonly=True,
+    )
 
 
 class ProductProductAgent(models.Model):
-    _name = 'product.product.agent'
+    _name = "product.product.agent"
 
     @api.multi
     def get_commission_id_product(self, product, agent):
         commission_id = False
         # commission_id for all agent
         for commission_all_agent in self.search(
-                [('product_id', '=', product), ('agent', '=', False)]):
-                    commission_id = commission_all_agent.commission.id
+            [("product_id", "=", product), ("agent", "=", False)]
+        ):
+            commission_id = commission_all_agent.commission.id
         # commission_id for agent
         for product_agent_id in self.search(
-                [('product_id', '=', product), ('agent', '=', agent.id)]):
-                    commission_id = product_agent_id.commission.id
+            [("product_id", "=", product), ("agent", "=", agent.id)]
+        ):
+            commission_id = product_agent_id.commission.id
         return commission_id
 
     product_id = fields.Many2one(
-        comodel_name="product.product",
-        required=True,
-        ondelete="cascade",
-        string="")
+        comodel_name="product.product", required=True, ondelete="cascade", string=""
+    )
     agent = fields.Many2one(
-        comodel_name="res.partner", required=False, ondelete="restrict",
-        domain="[('agent', '=', True')]")
+        comodel_name="res.partner",
+        required=False,
+        ondelete="restrict",
+        domain="[('agent', '=', True')]",
+    )
     commission = fields.Many2one(
-        comodel_name="sale.commission", required=True, ondelete="restrict")
+        comodel_name="sale.commission", required=True, ondelete="restrict"
+    )
 
     @api.multi
     def name_get(self):
@@ -50,6 +56,9 @@ class ProductProductAgent(models.Model):
         return res
 
     _sql_constraints = [
-        ('unique_agent', 'UNIQUE(product_id, agent)',
-         'You can only add one time each agent.')
+        (
+            "unique_agent",
+            "UNIQUE(product_id, agent)",
+            "You can only add one time each agent.",
+        )
     ]
