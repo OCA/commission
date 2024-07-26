39630322effe5dbc04a974c20665db64e9fc742e
Alessio Renda <renda855@gmail.com>
2024-07-26 08:52:51 +0200

    [IMP] sale_commission_partial_settlement: add maintainers

--- sale_commission_partial_settlement/__manifest__.py
+++ sale_commission_partial_settlement/__manifest__.py
@@ -1,8 +1,9 @@
 # Copyright 2023 Nextev
 {
     "name": "Sales commissions based on paid amount",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "author": "Nextev Srl," "Ooops," "Odoo Community Association (OCA)",
+    "maintainers": ["aleuffre", "renda-dev", "PicchiSeba"],
     "category": "Sales Management",
     "license": "AGPL-3",
     "depends": ["sale_commission"],
