1d3a71942bc928f570caf3900216768980e1251a
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2024-08-08 12:09:30 +0200

    [IMP] account_commission: Index on invoice_agent_line_id
    That way, deletions of agent lines in invoices are faster.

--- account_commission/__manifest__.py
+++ account_commission/__manifest__.py
@@ -3,7 +3,7 @@
 # Copyright 2014-2022 Tecnativa - Pedro M. Baeza
 {
     "name": "Account commissions",
-    "version": "15.0.3.3.1",
+    "version": "15.0.3.3.2",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "category": "Sales Management",
     "license": "AGPL-3",
