189d0e39e4df4092ea5b0394a1f2deecbc509414
Aungkokolin1997 <lin@quartile.co>
2024-10-10 02:22:42 +0000

    [IMP] commission: add the commission_ids field to the settlement and include it in the views

--- commission/models/commission_settlement.py
+++ commission/models/commission_settlement.py
@@ -60,6 +60,14 @@ class CommissionSettlement(models.Model):
         default=lambda self: self._default_company_id(),
         required=True,
     )
+    commission_ids = fields.Many2many(
+        comodel_name="commission", compute="_compute_commission_ids", store=True
+    )
+
+    @api.depends("line_ids.commission_id")
+    def _compute_commission_ids(self):
+        for rec in self:
+            rec.commission_ids = rec.line_ids.mapped("commission_id")
 
     def _default_currency_id(self):
         return self.env.company.currency_id.id
