0c950bc4300785ccf4835404a25af5b9a68d7355
Alessandro Uffreduzzi <alessandro.uffreduzzi@pytech.it>
2024-10-16 18:08:41 +0200

    [IMP] sale_commission_product_criteria: print comm value on report

--- sale_commission_product_criteria/__manifest__.py
+++ sale_commission_product_criteria/__manifest__.py
@@ -3,14 +3,15 @@
 {
     "name": "Sale Commission Product Criteria",
     "summary": "Advanced commissions rules",
-    "version": "14.0.1.1.5",
+    "version": "14.0.1.1.6",
     "author": "Ilyas, Ooops404, Odoo Community Association (OCA)",
-    "maintainers": ["ilyasProgrammer"],
+    "maintainers": ["ilyasProgrammer", "aleuffre", "renda-dev", "PicchiSeba"],
     "website": "https://github.com/OCA/commission",
     "category": "Sales Management",
     "license": "AGPL-3",
     "depends": ["sale_commission", "web_domain_field"],
     "data": [
+        "reports/report_settlement_template.xml",
         "views/views.xml",
         "security/ir.model.access.csv",
     ],
