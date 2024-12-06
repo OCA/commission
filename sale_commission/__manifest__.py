3c878fd9a79a0587b998e69bd4eb5367371e7fcd
Borruso <gborruso@dinamicheaziendali.it>
2024-12-06 18:04:00 +0100

    [MIG] sale_commission: Migration to 18.0

--- sale_commission/__manifest__.py
+++ sale_commission/__manifest__.py
@@ -3,7 +3,7 @@
 # Copyright 2014-2022 Tecnativa - Pedro M. Baeza
 {
     "name": "Sales commissions",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "category": "Sales Management",
     "license": "AGPL-3",
