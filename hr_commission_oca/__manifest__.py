f3fb67ee9bba0d9bdba5c15cea919262951e2295
matteo.tognini <matteo.tognini@nextev.it>
2025-04-11 10:30:33 +0200

    [MIG]hr_commission: migration to v18

--- hr_commission_oca/__manifest__.py
+++ hr_commission_oca/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "HR commissions",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/commission",
     "category": "Commissions",
