c539d2c4f54eae3e1389f90af23e5389a191ab7e
PolComas <pcomasro93@alumnes.ub.edu>
2025-07-22 10:43:26 +0200

    15.0][FIX] sale_commission_settlement_report: satisfy pre-commit (black/flake8)

--- sale_commission_settlement_report/tests/test_sale_commission_settlement_report.py
+++ sale_commission_settlement_report/tests/test_sale_commission_settlement_report.py
@@ -1,14 +1,18 @@
 def test_showable_line_ids_filter(self):
     company = self.env.company
-    settlement = self.env['commission.settlement'].create({...})
-    line0 = self.env['commission.settlement.line'].create({
-        'settlement_id': settlement.id,
-        'settled_amount': 0.0,
-    })
-    line1 = self.env['commission.settlement.line'].create({
-        'settlement_id': settlement.id,
-        'settled_amount': 10.0,
-    })
+    settlement = self.env["commission.settlement"].create({...})
+    line0 = self.env["commission.settlement.line"].create(
+        {
+            "settlement_id": settlement.id,
+            "settled_amount": 0.0,
+        }
+    )
+    line1 = self.env["commission.settlement.line"].create(
+        {
+            "settlement_id": settlement.id,
+            "settled_amount": 10.0,
+        }
+    )
 
     company.settlement_skip_zero_amount_lines = False
     settlement._compute_showable_line_ids()
