991c1b753d6665abf477e78140da7efe09c4f516
PolComas <pcomasro93@alumnes.ub.edu>
2025-08-01 11:27:12 +0200

    more tests

--- sale_commission_settlement_report/models/sale_commission_settlement_line.py
+++ sale_commission_settlement_report/models/sale_commission_settlement_line.py
@@ -17,5 +17,5 @@ class SaleCommissionSettlement(models.Model):
         for record in self:
             lines = record.line_ids
             if record.company_id.settlement_skip_zero_amount_lines:
-                lines = lines.filtered(lambda line: line.settled_amount)
+                lines = lines.filtered(key=lambda line: line.settled_amount)
             record.showable_line_ids = lines
