f20aee1dd980ec3dcd11a6fe1f0d7ba7a7430240
Felipe Motter <felipe@engenere.one>
2025-08-05 22:55:12 -0300

    [MIG] sale_commission_pricelist: backport-migration to 16.0

--- sale_commission_pricelist/__manifest__.py
+++ sale_commission_pricelist/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sales commissions by pricelist",
-    "version": "17.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Tecnativa, " "Odoo Community Association (OCA), " "ArcheTI",
     "category": "Sales Management",
     "website": "https://github.com/OCA/commission",
