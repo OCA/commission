1b43093209057a72b700646961195764626602f6
Juan Carlos <suker_juan@hotmail.com>
2026-06-15 23:17:01 +0200

    [MIG] commission_formula_oca: Migration to 19.0

--- commission_formula_oca/__manifest__.py
+++ commission_formula_oca/__manifest__.py
@@ -4,13 +4,13 @@
 # License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Commission Formula OCA",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Commission",
     "license": "AGPL-3",
     "summary": "Commissions computed by formulas",
     "author": "Abstract,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/commission",
-    "depends": ["commission_oca", "account"],
+    "depends": ["account_commission_oca"],
     "data": ["views/commission_view.xml"],
     "demo": ["demo/commission_demo.xml"],
     "assets": {
